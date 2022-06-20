42fc51137062d3139ca327689c16e88530f5c820
ronpysui <kevinnguyen602641@gmail.com>
2022-06-20 17:46:06 -0500

    6/20

--- main.py
+++ main.py
@@ -9,30 +9,35 @@ imagepathLoad=json.loads(data) #loads json file
 
 quantity=input('how many to generate?: ')
 
-#random pick
 count=0
 foldercount=5
+height,width=32,32
+nft_image=Image.new("RGBA",(height,width),(0,0,0,0))
+
+#loop random file
 while count<int(quantity):
     print("test")
     storeImages=list()
+
     #iterate through "Layers" dir
     for files in os.scandir(imagepathLoad["imagePaths"]["files"]):
         strFiles=str(files).replace("<DirEntry","").replace("'","").replace(">","").strip()
-        print(strFiles)
 
+        #pick random image
         random_image=random.choice([
             image for image in os.listdir(imagepathLoad["imagePaths"]["files"]+"\\"+strFiles)
             if os.path.isfile(os.path.join(imagepathLoad["imagePaths"]["files"]+"\\"+strFiles)) and image.endswith(".jpg")
             or image.endswith(".png")
         ])
-
-        #random img
-        im=Image.open(imagepathLoad["imagePaths"]["files"]+strFiles+"\\"+random_image)
-        nft_image=Image.new("RGBA",(32,32),(0,0,0,0))
-        nft_image.save(imagepathLoad["imagePaths"]["nfts"]+"\\"+"image.png","PNG")
-        #nft_image.show()
-
     count+=1
+
+    #save img --> dir
+    nft_image.save(imagepathLoad["imagePaths"]["nfts"]+"\\"+f"image_{count}.png","PNG")
+
+    #random img
+    randIm=Image.open(imagepathLoad["imagePaths"]["files"]+strFiles+"\\"+random_image)
+
+
 count=0
 
 print(foldercount,count)
